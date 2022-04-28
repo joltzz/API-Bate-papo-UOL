f3ecae96a280c82cf1f150b85309fd0d4ca68d15
joltzz <guijsilva02@gmail.com>
2022-04-28 14:31:00 -0300

    messages get() feito

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import Joi from "joi";
 import dotenv from "dotenv";
 
 
+//Configs
 dotenv.config();
 const server=express();
 server.use(cors());
 server.use(json());
 
+//MongoClient
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let dbChatUol;
 mongoClient.connect(() => {
@@ -19,7 +21,7 @@ mongoClient.connect(() => {
 });
 
 
-//* Participants
+// Participants
 server.get("/participants", async (req, res) => {
     try {
       const participantsCollection = dbChatUol.collection("participants");
@@ -36,6 +38,37 @@ server.post("/participants", async (req, res) => {
 });
 
 
+// Messages
+server.get("/messages", async (req, res) => {
+    const username = req.headers.user;
+    const limit = parseInt(req.query.limit);
+    try {
+        const chatCollection = dbChatUol.collection("messages");
+        const filterChat = await chatCollection
+        .find({
+            $or: [
+                { type: "message" },
+                { $and: [{ type: "private_message" }, { to: username }] },
+                { $and: [{ type: "private_message" }, { from: username }] },
+            ],
+        })
+        .toArray();
+        if (!limit) {
+            res.send(filterChat);
+        } else {
+            res.send(filterChat.slice(-limit));
+        }
+    } catch (error) {
+        res.sendStatus(500);
+    }
+});
+
+server.post("/messages", async (req, res) => {
+    //
+});
+
+
+// Listen
 server.listen(5005, ()=>{
     console.log(chalk.bgGreen("Programa Rodando!"))
 });
