f23577a2aef504a75862ed0d3cdd28855ad331c7
joltzz <guijsilva02@gmail.com>
2022-04-29 11:21:31 -0300

    iniciando joi e status pronto

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { MongoClient, ObjectId } from "mongodb";
 import cors from "cors";
 import chalk from "chalk";
 import dayjs from "dayjs";
-import Joi from "joi";
+import joi from "joi";
 import dotenv from "dotenv";
 
 
@@ -13,11 +13,24 @@ const server=express();
 server.use(cors());
 server.use(json());
 
+
 //MongoClient
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let dbChatUol;
 mongoClient.connect(() => {
   dbChatUol = mongoClient.db("chatuol");
+});
+
+
+//Schemas
+const userSchema=joi.object({
+    name: joi.string().required(),
+});
+
+const messageSchema=joi.object({
+    to: joi.string.required(),
+    text: joi.string().required(),
+    type: joi.valid("message", "private_message").required(),
 });
 
 
@@ -36,7 +49,6 @@ server.get("/participants", async (req, res) => {
 server.post("/participants", async (req, res) => {
     //
 });
-
 
 // Messages
 server.get("/messages", async (req, res) => {
@@ -66,6 +78,34 @@ server.get("/messages", async (req, res) => {
 server.post("/messages", async (req, res) => {
     //
 });
+
+
+//Status
+server.post("/status", async (req, res)=>{
+    const username= req.header.user;
+
+    try{
+        const participantsCollection =dbChatUol.collection("participants");
+        const participantsList = await participantsCollection.find({}).toArray();
+        if(!participantsList.find((p)=> p.name === username)){
+            res.sendStatus(404);
+        }
+        else{
+            await participantsCollection.updateOne(
+                { name: username},
+                { $set: {lastStatus: Date.now() }}
+            );
+            res.sendStatus(200);
+        }
+    } catch(e){
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+
+//Remove
+//setinterval()...
 
 
 // Listen
